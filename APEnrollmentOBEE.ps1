c98ac1085bd14edfdc0eb0bb23b2934d6268ac96
markorr321 <markorr321@gmail.com>
2023-03-02 16:16:23 -0600

    Create APEnrollmentOBEE.ps1

---
+++ APEnrollmentOBEE.ps1
@@ -0,0 +1,8 @@
+##AutPilot Enrollment from OBEE##
+
+Press Shift Function F10 
+Type powershell
+set-executionpolicy bypass
+install-script get-windowsautopilotinfo -force
+Get-WindowsAutoPilotInfo.ps1 -GroupTag field -Online 
+sysprep\sysprep.exe

93e84154596f884b273277acdc3447a71cd3c49d
markorr321 <markorr321@gmail.com>
2024-11-22 12:53:13 -0600

    Update APEnrollmentOBEE.ps1

--- APEnrollmentOBEE.ps1
+++ APEnrollmentOBEE.ps1
@@ -1,8 +1,20 @@
 ##AutPilot Enrollment from OBEE##
 
-Press Shift Function F10 
-Type powershell
+#Launch a command line
+Press Shift F10 
+Close the command line
+
+#Launch a Powershell Admin Window
+Windows R --> Type: Powershell --> Press Control Shift Enter
+
+#Set Executionpolicy
 set-executionpolicy bypass
+
+#Installs "Get-WindowsAutoPilotInfo Script
 install-script get-windowsautopilotinfo -force
+
+# Runs the Get-WindowsAutoPilotInfo script to gather device information for Windows AutoPilot and upload it to the Microsoft Endpoint Manager admin center (Intune)
 Get-WindowsAutoPilotInfo.ps1 -GroupTag field -Online 
+
+#Prepares the Windows installation for imaging or deployment
 sysprep\sysprep.exe
